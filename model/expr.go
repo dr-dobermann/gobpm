5e762ff213caf63cccec1fed48f54552648a58e0
dr-dobermann <rgabitov@gmail.com>
2021-10-21 23:38:11 +0600

    Add all VarStore necessity funcions
    NewBool, NewString, NewFloat added
    GetVar and DelVar added with testing

--- model/expr.go
+++ model/expr.go
@@ -12,7 +12,12 @@ const (
 	VtInt VarType = iota
 	VtBool
 	VtString
+	VtFloat
 )
+
+func (vt VarType) String() string {
+	return []string{"Int", "Bool", "String", "Float"}[vt]
+}
 
 type Variable struct {
 	BaseElement
@@ -20,6 +25,8 @@ type Variable struct {
 	initialized bool
 	vtype       VarType
 	value       interface{}
+	// float precision. Default 2
+	prec int
 }
 
 func (v *Variable) Name() string {
@@ -30,8 +37,20 @@ func (v *Variable) Type() VarType {
 	return v.vtype
 }
 
+func (v *Variable) Precision() int {
+	return v.prec
+}
+
+func (v *Variable) SetPrecision(p int) {
+	if p < 0 {
+		p = 0
+	}
+	v.prec = p
+}
+
 func (v *Variable) Int() int {
-	if v.vtype == VtString {
+	switch v.vtype {
+	case VtString:
 		s := v.value.(string)
 
 		if i, err := strconv.ParseInt(s, 10, 64); err != nil {
@@ -40,6 +59,11 @@ func (v *Variable) Int() int {
 		} else {
 			return int(i)
 		}
+
+	case VtFloat:
+		f := v.value.(float64)
+		return int(f)
+
 	}
 
 	i := v.value.(int)
@@ -63,6 +87,10 @@ func (v *Variable) String() string {
 			s = "false"
 		}
 
+	case VtFloat:
+		f := v.value.(float64)
+		s = strconv.FormatFloat(f, 'f', v.prec, 64)
+
 	case VtString:
 		s = v.value.(string)
 	}
@@ -83,6 +111,12 @@ func (v *Variable) Bool() bool {
 	case VtBool:
 		b = v.value.(bool)
 
+	case VtFloat:
+		f := v.value.(float64)
+		if f == 0.0 {
+			b = false
+		}
+
 	case VtString:
 		s := v.value.(string)
 		if len(s) > 0 {
@@ -91,6 +125,38 @@ func (v *Variable) Bool() bool {
 	}
 
 	return b
+}
+
+func (v *Variable) Float64() float64 {
+	var (
+		f   float64
+		err error
+	)
+
+	switch v.vtype {
+	case VtInt:
+		i := v.value.(int)
+		f = float64(i)
+
+	case VtBool:
+		b := v.value.(bool)
+		if b {
+			f = 1.0
+		}
+
+	case VtFloat:
+		f = v.value.(float64)
+
+	case VtString:
+		s := v.value.(string)
+		f, err = strconv.ParseFloat(s, 64)
+		if err != nil {
+			panic("couldn't transform string " +
+				s + " into float64 : " + err.Error())
+		}
+	}
+
+	return f
 }
 
 type VPack struct {
@@ -116,38 +182,128 @@ type varMap map[VarType]*Variable
 // varStore retpresents the variables store
 // there is one global store and stores in every single process instance
 // variables could have the same name but they should be different by value type
-type varStore map[string]varMap
+type VarStore map[string]varMap
 
-func (vs *varStore) getVar(vn string, vt VarType) *Variable {
+var global VarStore = make(VarStore)
 
+func (vs *VarStore) getVar(vn string, vt VarType, returnEmpty bool) *Variable {
 	vm := map[string]varMap(*vs)[vn]
 	if vm == nil {
+		if !returnEmpty {
+			return nil
+		}
+
 		vm = make(varMap)
+		map[string]varMap(*vs)[vn] = vm
 	}
 
-	v := map[VarType]*Variable(vm)[vt]
-	if v == nil {
+	v, ok := map[VarType]*Variable(vm)[vt]
+
+	if !ok {
+		if !returnEmpty {
+			return nil
+		}
+
 		v = &Variable{
 			BaseElement: BaseElement{
 				id:            NewID(),
 				Documentation: Documentation{"", ""}},
-			name: vn}
+			name:  vn,
+			vtype: vt,
+			prec:  2}
+
+		map[VarType]*Variable(vm)[vt] = v
 	}
 
 	return v
 }
 
-var global varStore = make(varStore)
+func (vs *VarStore) GetVar(vn string, vt VarType) (*Variable, error) {
+	v := vs.getVar(vn, vt, false)
+	if v == nil {
+		return nil, NewModelError(uuid.Nil,
+			"variable "+vn+" of type "+vt.String()+" isn't found", nil)
+	}
+
+	return v, nil
+}
+
+func (vs *VarStore) DelVar(vn string, vt VarType) error {
+	vm := map[string]varMap(*vs)[vn]
+	if vm == nil {
+		return NewModelError(uuid.Nil,
+			"couldn't find variable group "+vn,
+			nil)
+	}
+	if _, ok := map[VarType]*Variable(vm)[vt]; !ok {
+		return NewModelError(uuid.Nil,
+			"couldn't find variable "+vn+" of type "+vt.String(),
+			nil)
+	}
+	delete(map[VarType]*Variable(vm), vt)
+
+	// if there is no variables in var map, then delete the
+	// variable map itself
+	if len(vm) == 0 {
+		delete(map[string]varMap(*vs), vn)
+	}
+
+	return nil
+}
 
 // NewInt creates a new int variable
 // if the variable exists, the error returned
-func (vs *varStore) NewInt(vn string, val int) (*Variable, error) {
-
-	v := global.getVar(vn, VtInt)
+func (vs *VarStore) NewInt(vn string, val int) (*Variable, error) {
+	v := vs.getVar(vn, VtInt, true)
 
 	if v.initialized {
 		return nil, NewModelError(uuid.Nil,
-			"variable "+vn+" of int type already exists",
+			"variable "+vn+" of Int type already exists",
+			nil)
+	}
+
+	v.value = val
+	v.initialized = true
+
+	return v, nil
+}
+
+func (vs *VarStore) NewBool(vn string, val bool) (*Variable, error) {
+	v := vs.getVar(vn, VtBool, true)
+
+	if v.initialized {
+		return nil, NewModelError(uuid.Nil,
+			"variable "+vn+" of Bool type already exists",
+			nil)
+	}
+
+	v.value = val
+	v.initialized = true
+
+	return v, nil
+}
+
+func (vs *VarStore) NewString(vn string, val string) (*Variable, error) {
+	v := vs.getVar(vn, VtString, true)
+
+	if v.initialized {
+		return nil, NewModelError(uuid.Nil,
+			"variable "+vn+" of String type already exists",
+			nil)
+	}
+
+	v.value = val
+	v.initialized = true
+
+	return v, nil
+}
+
+func (vs *VarStore) NewFloat(vn string, val float64) (*Variable, error) {
+	v := vs.getVar(vn, VtFloat, true)
+
+	if v.initialized {
+		return nil, NewModelError(uuid.Nil,
+			"variable "+vn+" of Float type already exists",
 			nil)
 	}
 
--- model/expr_test.go
+++ model/expr_test.go
@@ -1,12 +1,18 @@
 package model
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestVariables(t *testing.T) {
+func TestVariablesValues(t *testing.T) {
+	// int variable
 	v, err := global.NewInt("x", 2)
-
 	if err != nil {
 		t.Error("Couldn't create a new variable ", err)
+	}
+
+	if _, err = global.NewInt("x", 3); err == nil {
+		t.Error("Double variable in namespace")
 	}
 
 	if v == nil {
@@ -26,5 +32,67 @@ func TestVariables(t *testing.T) {
 	b := v.Bool()
 	if !b {
 		t.Error("invalid bool variable value : ", b)
+	}
+
+	f := v.Float64()
+	if f != 2.0 {
+		t.Error("invalid float64 variable value : ", f)
+	}
+}
+
+func TestVariableGetter(t *testing.T) {
+	global.NewInt("x", 2)
+	v1, err := global.GetVar("x", VtInt)
+	if v1 == nil || err != nil {
+		t.Error("couldn't get a variable : ", err)
+	}
+
+	if v1 == nil || v1.Int() != 2 {
+		t.Error("invalid variable value")
+	}
+
+	if _, err := global.GetVar("xx", VtInt); err == nil {
+		t.Error("non-existed variable returned")
+	}
+
+	v2, err := global.NewBool("x", true)
+	if err != nil {
+		t.Error("couldn't add new variable", err)
+	}
+	if v2.Bool() != true {
+		t.Error("invalid variable value")
+	}
+}
+
+func TestVariableDeleter(t *testing.T) {
+	global.NewInt("xx", 3)
+	global.NewBool("xx", true)
+
+	if err := global.DelVar("xx", VtBool); err != nil {
+		t.Error("couldn't delete variable")
+	}
+
+	if err := global.DelVar("xx", VtBool); err == nil {
+		t.Error("double deleting")
+	}
+
+	if _, err := global.GetVar("xx", VtBool); err == nil {
+		t.Error("variable isn't deleted")
+	}
+
+	if v, err := global.GetVar("xx", VtInt); err != nil {
+		t.Error("variable isn't found")
+	} else {
+		if i := v.Int(); i != 3 {
+			t.Error("invalid variable value ", i)
+		}
+	}
+
+	if err := global.DelVar("xxx", VtInt); err == nil {
+		t.Error("deleting inexisted variable")
+	}
+
+	if err := global.DelVar("xx", VtInt); err != nil {
+		t.Error("couldn't delete variable", err)
 	}
 }
