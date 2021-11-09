59c6fd9b3813c87b30adcfc1d4d259503dd94723
dr-dobermann <rgabitov@gmail.com>
2021-11-09 07:16:44 +0600

    Add ProcessExecutionError

--- thresher/thresher.go
+++ thresher/thresher.go
@@ -24,8 +24,10 @@ type TrackState uint8
 const (
 	TsCreated TrackState = iota
 	TsStarted
+	// Intermediate
 	TsAwaitsService
 	TsAwaitsMessage
+	// Final status
 	TsMerged
 	TsEnded
 	TsError
@@ -44,8 +46,36 @@ type track struct {
 	node     model.Node
 }
 
-// newTrack creates a new track from single Node
+type ProcessExecutingError struct {
+	piID    model.Id
+	pID     model.Id
+	trackId model.Id
+	msg     string
+	Err     error
+}
+
+func (pee ProcessExecutingError) Error() string {
+	return fmt.Sprintf("%s[%s]:%s: %s : %v",
+		pee.piID, pee.pID, pee.trackId, pee.msg, pee.Err)
+}
+
+func NewProcExecError(trk *track, msg string, err error) ProcessExecutingError {
+	pee := ProcessExecutingError{msg: msg, Err: err}
+
+	if trk != nil {
+		pee.trackId = trk.id
+		pee.piID = trk.instance.id
+		pee.pID = trk.instance.snapshot.ID()
+	}
+
+	return pee
+}
+
+// newTrack creates a new track started from a Node n.
 func newTrack(n model.Node, inst *ProcessInstance) *track {
+	if n == nil {
+		panic("couldn't start track from nil Node")
+	}
 
 	t := &track{
 		id:       model.NewID(),
@@ -55,6 +85,10 @@ func newTrack(n model.Node, inst *ProcessInstance) *track {
 	return t
 }
 
+// tick make a single step on track if current node isn't started
+// yet.
+// Executed node returns its new status. if status is from Final ones
+// the Node returns a list of valid outcomes flows.
 func (tr *track) tick(ctx context.Context) error {
 
 	if tr.state != TsCreated {
@@ -86,8 +120,38 @@ func (tr *track) tick(ctx context.Context) error {
 	return nil
 }
 
+// updateState updates the current track state onto ns.
+// if it's a Final staus and ff isn't empty the next Node selected and
+// new tracks might be created.
+// if it's a Finel status and there are no more flows, the track is ended.
+func (tr *track) updateState(ns TrackState, ff []*model.SequenceFlow) error {
+	if tr.state == TsEnded || tr.state == TsError || tr.state == TsMerged {
+		return NewProcExecError(
+			tr, "couldn't update state on finalized track", nil)
+	}
+
+	switch ns {
+	case TsCreated:
+		return NewProcExecError(
+			tr, "couldn't set a Created status on executing Node", nil)
+
+	case TsStarted:
+
+	// Intermediate statuses
+	case TsAwaitsMessage, TsAwaitsService:
+
+	// Final statuses
+	case TsEnded:
+
+	case TsError:
+	}
+
+	return nil
+}
+
 // ProcessInstance represents a single run-time process instance
 type ProcessInstance struct {
+	id model.Id
 	// the copy of the process model the instance is based on
 	snapshot *model.Process
 	vs       model.VarStore
