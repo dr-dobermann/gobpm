8689fb09121a8669c561547a3230d5c331af88aa
dr-dobermann <rgabitov@gmail.com>
2022-01-30 09:34:57 +0600

    GEP: Add notEqual test

--- pkg/expression/gep/operations/cond_test.go
+++ pkg/expression/gep/operations/cond_test.go
@@ -5,9 +5,56 @@ import (
 	"testing"
 	"time"
 
+	"github.com/dr-dobermann/gobpm/pkg/expression/gep"
 	vars "github.com/dr-dobermann/gobpm/pkg/variables"
 	"github.com/matryer/is"
 )
+
+func TestConditions(t *testing.T) {
+	for _, tn := range []string{"equal", "notEqual", "less", "greater", "le", "ge"} {
+		testCondition(t, tn)
+	}
+}
+
+func testCondition(t *testing.T, condName string) {
+	is := is.New(t)
+
+	testDescr, ok := tests[condName]
+	is.True(ok)
+
+	for _, tc := range testDescr.testCases {
+		testName := fmt.Sprintf("%s: %v(%v) %s %v(%v)",
+			condName, tc.xt, tc.xv, testDescr.opStr, tc.yt, tc.yv)
+
+		t.Run(testName, func(t *testing.T) {
+			x := vars.V(tc.xt.String(), tc.xt, tc.xv)
+			y := vars.V(tc.yt.String(), tc.yt, tc.yv)
+
+			condOp, err := testDescr.condFunc(y, "res")
+			is.NoErr(err)
+			is.True(condOp != nil)
+
+			res, err := condOp(x)
+			if err != nil {
+				if tc.testType == shouldFail {
+					return
+				}
+
+				t.Fatal("test should pass, but have error:", err)
+			}
+
+			if tc.testType == shouldFail {
+				t.Fatal("test should fail")
+			}
+
+			is.True(res != nil)
+
+			if !checkRes(tc.xt, tc.resValue, *res) {
+				t.Fatalf("result %v doesn't meet expectation %v", res.VariableValues, tc.resName)
+			}
+		})
+	}
+}
 
 var (
 	strTime = "1973-02-23T05:15:00+06:00"
@@ -55,7 +102,8 @@ var (
 		{vars.Float, 7, vars.String, "trash", "res", vars.VariableValues{B: false}, shouldFail},
 		{vars.Float, -7.90, vars.Float, -7.9, "res", vars.VariableValues{B: true}, shouldPass},
 		{vars.Float, 12, vars.Float, 0.0, "res", vars.VariableValues{B: false}, shouldPass},
-		{vars.Float, 3, vars.Time, timeTest, "res", vars.VariableValues{B: true}, shouldFail},
+		{vars.Float, 3, vars.Time, timeTest, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Float, float64(timeTest.UnixMilli()), vars.Time, timeTest, "res", vars.VariableValues{B: false}, shouldPass},
 
 		{vars.Time, timeTest, vars.Int, 10, "res", vars.VariableValues{B: false}, shouldPass},
 		{vars.Time, timeTest, vars.Int, timeTest.UnixMilli(), "res", vars.VariableValues{B: true}, shouldPass},
@@ -67,40 +115,72 @@ var (
 		{vars.Time, timeTest, vars.Time, timeTest, "res", vars.VariableValues{B: true}, shouldPass},
 		{vars.Time, timeTest, vars.Time, time.Now(), "res", vars.VariableValues{B: false}, shouldPass},
 	}
-)
 
-func TestEqual(t *testing.T) {
-	is := is.New(t)
+	notEqualTests = []testCase{
+		{vars.Int, 5, vars.Int, 5, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Int, 5, vars.Int, 7, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Int, 5, vars.Bool, false, "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Int, 5, vars.String, "5", "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Int, 5, vars.String, "7", "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Int, 5, vars.String, "trash", "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Int, 5, vars.Float, -7.9, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Int, 5, vars.Float, 4.9, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Int, timeTest.UnixMilli(), vars.Time, timeTest, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Int, 5, vars.Time, time.Now(), "res", vars.VariableValues{B: true}, shouldPass},
 
-	for _, tc := range equalTests {
-		testName := fmt.Sprintf("%v(%v) == %v(%v)", tc.xt, tc.xv, tc.yt, tc.yv)
+		{vars.Bool, true, vars.Int, 5, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Bool, false, vars.Int, 7, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Bool, true, vars.Int, 0, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Bool, true, vars.Bool, false, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Bool, false, vars.Bool, false, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Bool, true, vars.String, "true", "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Bool, true, vars.String, "false", "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Bool, true, vars.String, "trash", "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Bool, true, vars.Float, -7.9, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Bool, true, vars.Float, 0, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Bool, true, vars.Time, timeTest, "res", vars.VariableValues{B: false}, shouldFail},
 
-		t.Run(testName, func(t *testing.T) {
-			x := vars.V(tc.xt.String(), tc.xt, tc.xv)
-			y := vars.V(tc.yt.String(), tc.yt, tc.yv)
+		{vars.String, "5", vars.Int, 5, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.String, "5", vars.Int, 7, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.String, "true", vars.Bool, false, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.String, "true", vars.Bool, true, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.String, "trash", vars.Bool, false, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.String, "5", vars.String, "5", "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.String, "5", vars.String, "7", "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.String, "5", vars.Float, -7.9, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.String, "5", vars.Float, 4.9, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.String, strTime, vars.Time, timeTest, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.String, strTime, vars.Time, time.Now(), "res", vars.VariableValues{B: true}, shouldPass},
 
-			equalOp, err := Equal(y, "res")
-			is.NoErr(err)
-			is.True(equalOp != nil)
+		{vars.Float, 5, vars.Int, 5, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Float, 5, vars.Int, 7, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Float, 5, vars.Bool, false, "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Float, 5, vars.String, "5", "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Float, 5, vars.String, "7", "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Float, 5, vars.String, "trash", "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Float, 5, vars.Float, -7.9, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Float, 4.9, vars.Float, 4.9, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Float, float64(timeTest.UnixMilli()), vars.Time, timeTest, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Float, 5, vars.Time, time.Now(), "res", vars.VariableValues{B: true}, shouldPass},
 
-			res, err := equalOp(x)
-			if err != nil {
-				if tc.testType == shouldFail {
-					return
-				}
-
-				t.Fatal("test should pass, but have error:", err)
-			}
-
-			if tc.testType == shouldFail {
-				t.Fatal("test should fail")
-			}
-
-			is.True(res != nil)
-
-			if !checkRes(tc.xt, tc.resValue, *res) {
-				t.Fatalf("result %v doesn't meet expectation %v", res.VariableValues, tc.resName)
-			}
-		})
+		{vars.Time, time.Time{}, vars.Int, 0, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Time, time.Now(), vars.Int, 7, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Time, time.Now(), vars.Bool, false, "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Time, timeTest, vars.String, strTime, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Time, time.Now(), vars.String, strTime, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Time, time.Now(), vars.String, "trash", "res", vars.VariableValues{I: -1}, shouldFail},
+		{vars.Time, time.Now(), vars.Float, -7.9, "res", vars.VariableValues{B: true}, shouldPass},
+		{vars.Time, timeTest, vars.Float, float64(timeTest.UnixMilli()), "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Time, timeTest, vars.Time, timeTest, "res", vars.VariableValues{B: false}, shouldPass},
+		{vars.Time, timeTest, vars.Time, time.Now(), "res", vars.VariableValues{B: true}, shouldPass},
 	}
-}
+
+	tests = map[string]struct {
+		condFunc  func(y *vars.Variable, resName string) (gep.OpFunc, error)
+		testCases []testCase
+		opStr     string
+	}{
+		"equal":    {condFunc: Equal, testCases: equalTests, opStr: "=="},
+		"notEqual": {condFunc: NotEqual, testCases: notEqualTests, opStr: "!="},
+	}
+)
