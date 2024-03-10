0a303d8db656b15770475bf25ac7ab964b376680
dr-dobermann <rgabitov@gmail.com>
2024-03-10 21:48:02 +0500

    Add configurable Id Generator
    Added default random generator.
    Removed UUID generator.
    Added interface for custom random generator in foundation.

---
+++ pkg/model/foundation/idgen.go
@@ -0,0 +1,65 @@
+package foundation
+
+import (
+	"math/rand/v2"
+	"strconv"
+	"time"
+
+	"github.com/dr-dobermann/gobpm/pkg/errs"
+)
+
+var (
+	generator IdGenerator
+)
+
+// IdGenerator creates a new Id every time Generate called.
+type IdGenerator interface {
+	Generate() string
+}
+
+type GenFunc func() string
+
+func (gf GenFunc) Generate() string {
+	return gf()
+}
+
+// SetGenerator sets user-defined Id generator.
+func SetGenerator(newGen IdGenerator) error {
+	if newGen == nil {
+		return &errs.ApplicationError{
+			Message: "generator couldn't be empty",
+			Classes: []string{
+				errorClass,
+				errs.InvalidParameter}}
+	}
+
+	generator = newGen
+
+	return nil
+}
+
+// GenerateId returns a new Id from generator. If there is no generator, then
+// default Genereator will be used.
+func GenerateId() string {
+	if generator == nil {
+		SetGenerator(newDefaultGenerator())
+	}
+
+	return generator.Generate()
+}
+
+// ------------------- Default Generator ---------------------------------------
+// defaultIdGenerator is a default based on math/rand/v2 Id generator.
+type defaultIdGenerator struct {
+	r *rand.Rand
+}
+
+func newDefaultGenerator() *defaultIdGenerator {
+	return &defaultIdGenerator{
+		r: rand.New(rand.NewPCG(uint64(time.Now().UnixMilli()), 42)),
+	}
+}
+
+func (g *defaultIdGenerator) Generate() string {
+	return strconv.Itoa(int(g.r.Int64()))
+}
---
+++ pkg/model/foundation/idgen_test.go
@@ -0,0 +1,39 @@
+package foundation_test
+
+import (
+	"testing"
+
+	"github.com/dr-dobermann/gobpm/pkg/model/foundation"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func GenerateUUID() string {
+	return uuid.New().String()
+}
+
+func TestGenerator(t *testing.T) {
+	id1 := foundation.GenerateId()
+	require.NotEmpty(t, id1)
+	t.Log(id1)
+
+	id2 := foundation.GenerateId()
+	require.NotEmpty(t, id2)
+	t.Log(id2)
+
+	require.NotEqual(t, id1, id2)
+
+	foundation.SetGenerator(
+		foundation.GenFunc(GenerateUUID))
+
+	id1 = foundation.GenerateId()
+	require.NotEmpty(t, id1)
+	t.Log(id1)
+
+	id2 = foundation.GenerateId()
+	require.NotEmpty(t, id2)
+	t.Log(id2)
+
+	require.NotEqual(t, id1, id2)
+
+}
