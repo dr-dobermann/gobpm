9a20c52798ed120e2032a403aaad5c9360648820
dr-dobermann <rgabitov@gmail.com>
2024-11-12 09:37:18 +0500

    Add task association tests

--- pkg/model/activities/task_test.go
+++ pkg/model/activities/task_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/dr-dobermann/gobpm/pkg/model/data"
 	"github.com/dr-dobermann/gobpm/pkg/model/data/values"
 	"github.com/dr-dobermann/gobpm/pkg/model/flow"
+	"github.com/dr-dobermann/gobpm/pkg/model/foundation"
 	"github.com/dr-dobermann/gobpm/pkg/model/options"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -53,64 +54,169 @@ func TestOptions(t *testing.T) {
 func TestTaskData(t *testing.T) {
 	require.NoError(t, data.CreateDefaultStates())
 
-	type User struct {
-		Name string
-		Age  uint
-	}
+	t.Run("properties",
+		func(t *testing.T) {
+			type User struct {
+				Name string
+				Age  uint
+			}
 
-	props := []*data.Property{
-		data.MustProperty(
-			"x",
-			data.MustItemDefinition(
-				values.NewVariable(42)),
-			data.ReadyDataState),
-		data.MustProperty(
-			"user",
-			data.MustItemDefinition(
-				values.NewVariable(User{
-					Name: "Jon Doe",
-					Age:  27,
-				})),
-			data.ReadyDataState),
-	}
+			props := []*data.Property{
+				data.MustProperty(
+					"x",
+					data.MustItemDefinition(
+						values.NewVariable(42)),
+					data.ReadyDataState),
+				data.MustProperty(
+					"user",
+					data.MustItemDefinition(
+						values.NewVariable(User{
+							Name: "Jon Doe",
+							Age:  27,
+						})),
+					data.ReadyDataState),
+			}
 
-	task, err := activities.NewTask(
-		"test",
-		data.WithProperties(props...),
-		activities.WithoutParams())
-	require.NoError(t, err)
+			task, err := activities.NewTask(
+				"test",
+				data.WithProperties(props...),
+				activities.WithoutParams())
+			require.NoError(t, err)
 
-	s := mockscope.NewMockScope(t)
-	s.On("LoadData", task, mock.AnythingOfType("*data.Property"), mock.AnythingOfType("*data.Property")).
-		Return(
-			func(ndl scope.NodeDataLoader, dd ...data.Data) error {
-				for _, d := range dd {
-					t.Log("   >> got data: ", d.Name())
+			s := mockscope.NewMockScope(t)
+			s.On("LoadData", task, mock.AnythingOfType("*data.Property"), mock.AnythingOfType("*data.Property")).
+				Return(
+					func(ndl scope.NodeDataLoader, dd ...data.Data) error {
+						for _, d := range dd {
+							t.Log("   >> got data: ", d.Name())
 
-					p, ok := d.(*data.Property)
-					if !ok {
-						return fmt.Errorf("couldn't convert data %q to Property", d.Name())
-					}
+							p, ok := d.(*data.Property)
+							if !ok {
+								return fmt.Errorf("couldn't convert data %q to Property", d.Name())
+							}
 
-					if idx := slices.IndexFunc(
-						props,
-						func(prop *data.Property) bool {
-							return p.Id() == prop.Id()
-						}); idx == -1 {
-						return fmt.Errorf("couldn't find property %q", d.Name())
-					}
-				}
+							if idx := slices.IndexFunc(
+								props,
+								func(prop *data.Property) bool {
+									return p.Id() == prop.Id()
+								}); idx == -1 {
+								return fmt.Errorf("couldn't find property %q", d.Name())
+							}
+						}
 
-				return nil
-			})
+						return nil
+					})
 
-	dp, err := scope.NewDataPath("/task")
-	require.NoError(t, err)
+			dp, err := scope.NewDataPath("/task")
+			require.NoError(t, err)
 
-	err = task.RegisterData(dp, s)
-	require.NoError(t, err)
+			err = task.RegisterData(dp, s)
+			require.NoError(t, err)
 
-	// TODO: add associations to test
-	require.NoError(t, task.LoadData(context.Background()))
-	require.NoError(t, task.UploadData(context.Background(), s))
+			require.NoError(t, task.LoadData(context.Background()))
+			require.NoError(t, task.UploadData(context.Background(), s))
+		})
+
+	t.Run("data associations",
+		func(t *testing.T) {
+			input := data.MustItemAwareElement(
+				data.MustItemDefinition(
+					values.NewVariable(42),
+					foundation.WithId("x")),
+				nil)
+
+			output := data.MustItemAwareElement(
+				data.MustItemDefinition(
+					values.NewVariable(84),
+					foundation.WithId("x")),
+				data.ReadyDataState)
+
+			task, err := activities.NewTask(
+				"Task 1",
+				activities.WithSet(
+					"inputs",
+					"input_set_id",
+					data.Input,
+					data.DefaultSet,
+					[]*data.Parameter{
+						data.MustParameter(
+							"x",
+							input),
+					}),
+				activities.WithSet(
+					"outputs",
+					"output_set_id",
+					data.Output,
+					data.DefaultSet,
+					[]*data.Parameter{
+						data.MustParameter(
+							"x",
+							output),
+					}))
+			require.NoError(t, err)
+
+			// check input binding
+			ia, err := data.NewAssociation(
+				input,
+				data.WithSource(
+					data.MustItemAwareElement(
+						data.MustItemDefinition(
+							values.NewVariable(100),
+							foundation.WithId("x")),
+						data.ReadyDataState)))
+			require.NoError(t, err)
+
+			err = task.BindIncoming(ia)
+			require.NoError(t, err)
+
+			v, err := ia.Value(context.Background())
+			require.NoError(t, err)
+			require.Equal(t, 100, v.Structure().Get())
+
+			require.NoError(t, err)
+
+			// check inputs
+			ipp := task.Inputs()
+			require.Len(t, ipp, 1)
+			require.True(t,
+				slices.ContainsFunc(
+					ipp,
+					func(iae *data.ItemAwareElement) bool {
+						return iae.ItemDefinition().Id() == "x"
+					}))
+
+			require.Equal(t, 100, ipp[0].ItemDefinition().Structure().Get())
+
+			// check output binding
+			outRes := data.MustItemAwareElement(
+				data.MustItemDefinition(
+					values.NewVariable(0),
+					foundation.WithId("x")),
+				nil)
+
+			oa, err := data.NewAssociation(
+				outRes,
+				data.WithSource(output))
+			require.NoError(t, err)
+
+			err = task.BindOutgoing(oa)
+			require.NoError(t, err)
+
+			vo, err := oa.Value(context.Background())
+			require.NoError(t, err)
+			require.Equal(t, 84, vo.Structure().Get())
+
+			// check outputs
+			opp := task.Outputs()
+			require.Len(t, opp, 1)
+
+			require.True(t,
+				slices.ContainsFunc(
+					opp,
+					func(iae *data.ItemAwareElement) bool {
+						return iae.ItemDefinition().Id() == "x"
+					}))
+
+			require.Equal(t, 84, opp[0].ItemDefinition().Structure().Get())
+		})
 }
